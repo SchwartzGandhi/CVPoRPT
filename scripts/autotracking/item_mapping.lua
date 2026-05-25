9d9f08897d932d784a2bfd44d4071a8b3e52143c
Schwartz Gandhi <155924247+SchwartzGandhi@users.noreply.github.com>
2026-05-25 09:10:50 -0400

    locations hotfixes + autotracking

--- scripts/autotracking/item_mapping.lua
+++ scripts/autotracking/item_mapping.lua
@@ -3,11 +3,27 @@
 -- here are the SM items as an example: https://github.com/Cyb3RGER/sm_ap_tracker/blob/main/scripts/autotracking/item_mapping.lua
 BASE_ITEM_ID = 0
 ITEM_MAPPING = {
-	[BASE_ITEM_ID + 00000] = { { "toggle" } },
-	[BASE_ITEM_ID + 00001] = { { "progressive" } },
-	[BASE_ITEM_ID + 00002] = { { "consumable" } },
-	-- handle progressive_toggle as toggle, only changing it's active state
-	[BASE_ITEM_ID + 00003] = { { "progressive_toggle", "toggle" } },
-	-- multiple items on this id, add the consumable 3 times
-	[BASE_ITEM_ID + 00004] = { { "toggle" }, { "consumable", nil, 3 } }
+	[BASE_ITEM_ID + 590] = {{"toon", "toggle"}},
+	[BASE_ITEM_ID + 1795] = {{"locket", "toggle"}},
+	[BASE_ITEM_ID + 2049] = {{"puppet", "toggle"}},
+	[BASE_ITEM_ID + 2087] = {{"froggy", "toggle"}},
+	[BASE_ITEM_ID + 2088] = {{"birdie", "toggle"}},
+	[BASE_ITEM_ID + 2089] = {{"longspell", "toggle"}},
+	[BASE_ITEM_ID + 2090] = {{"fast", "toggle"}},
+	[BASE_ITEM_ID + 2140] = {{"tag", "toggle"}},
+	[BASE_ITEM_ID + 2141] = {{"doubleup", "toggle"}},
+	[BASE_ITEM_ID + 2142] = {{"subskill", "toggle"}},
+	[BASE_ITEM_ID + 2143] = {{"stay", "toggle"}},
+	[BASE_ITEM_ID + 2144] = {{"mario", "toggle"}},
+	[BASE_ITEM_ID + 2145] = {{"powerglove", "toggle"}},
+	[BASE_ITEM_ID + 2146] = {{"slide", "toggle"}},
+	[BASE_ITEM_ID + 2147] = {{"djump", "toggle"}},
+	[BASE_ITEM_ID + 2148] = {{"zip", "toggle"}},
+	[BASE_ITEM_ID + 2149] = {{"titansmitt", "toggle"}},
+	[BASE_ITEM_ID + 2150] = {{"roll", "toggle"}},
+	[BASE_ITEM_ID + 2151] = {{"cfalcfair", "toggle"}},
+	[BASE_ITEM_ID + 2152] = {{"kh2", "toggle"}},
+	[BASE_ITEM_ID + 2153] = {{"dangle", "toggle"}},
+	[BASE_ITEM_ID + 2154] = {{"downslash", "toggle"}},
+	[BASE_ITEM_ID + 2155] = {{"names", "toggle"}},
 }
